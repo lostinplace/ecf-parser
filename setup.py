3066636056bcaf728b04a9646620156603e40ccc
Chris Wheeler <cwwhee@amazon.com>
2018-03-29 14:59:26 -0400

    uploaded to pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ecf_parser',
+  packages = ['ecf_parser'],
+  version = '0.1',
+  description = "Tools for parsing Empyrion's .ecf files",
+  author = 'Chris Wheeler',
+  author_email = 'cmwhee@gmail.com',
+  url = 'https://github.com/lostinplace/ecf-parser',
+  download_url = 'https://github.com/lostinplace/ecf-parser/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['empyrion', 'modding'],
+  classifiers = [],
+)
